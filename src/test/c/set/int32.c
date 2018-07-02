a3d8ff8b561120f7335eca98a4282d09fbd5bdb1
Wilson Lin <contact@wilsonl.in>
2018-07-02 22:34:26 +1200

    Update int32 set test

--- src/test/c/set/int32.c
+++ src/test/c/set/int32.c
@@ -2,11 +2,7 @@
 #include "../../../main/c/set/int32.h"
 
 int main(void) {
-  nh_id_t set1 = nh_set_int32_create();
-  printf("Created set 1 with ID: %ld\n", set1);
-
-  nh_id_t set2 = nh_set_int32_create();
-  printf("Created set 2 with ID: %ld\n", set2);
+  nh_set_int32_t set1 = nh_set_int32_create();
 
   nh_set_int32_add(set1, 42);
   printf("Added 42\n");
