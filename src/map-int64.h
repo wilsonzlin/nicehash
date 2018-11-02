a0e646b5c583ef338fac7d485449880e6bc3fb0c
Wilson Lin <contact@wilsonl.in>
2018-11-02 17:54:59 +1300

    Add get_or_default to map

--- src/map-int64.h
+++ src/map-int64.h
@@ -4,4 +4,4 @@
 #include "./map.h"
 
 #define NH_MAP_INT64(value_name, value_t) \
-  NH_MAP(INT64, nh_map_int64_##value_name, int64_t, value_t)
+  NH_MAP(INT64, nh_map_int64_##value_name, int64_t, value_t, int64_t, -9223372036854775808)
