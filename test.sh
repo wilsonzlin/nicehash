2d063d8394fbbbebab57302ca3814183c0a68dd6
Wilson Lin <contact@wilsonl.in>
2018-11-04 01:10:09 +1300

    Test runner

---
+++ test.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+pushd "$(dirname "$0")/test" > /dev/null
+
+for t in *.c; do
+  if [ "$t" == _* ]; then
+    continue
+  fi
+
+  echo "Running test $t..."
+  tmp="$(mktemp)"
+  gcc --std=c11 -Wall -Wextra -Werror -O0 -g -o "$tmp" "$t"
+  if [ $? -ne 0 ]; then
+    echo "Compiling $t failed"
+    exit 2
+  fi
+  "$tmp"
+  result=$?
+  rm "$tmp"
+  if [ $result -ne 0 ]; then
+    echo "Test $t failed"
+    exit 1
+  fi
+done
+
+echo "All tests passed successfully"
+
+popd > /dev/null
