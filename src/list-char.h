8e2d49801b0bca639021de8d043a574cbe1d9a9b
Wilson Lin <contact@wilsonl.in>
2018-11-05 19:25:17 +1300

    Fix typo

--- src/list-char.h
+++ src/list-char.h
@@ -11,7 +11,7 @@ int nh_list_char_compare(nh_list_char* a, nh_list_char* b)
 	return strcmp(nh_list_char_underlying(a), nh_list_char_underlying(b));
 }
 
-int hbu_list_char_compare_lit(nh_list_char* a, char* const b)
+int nh_list_char_compare_lit(nh_list_char* a, char* const b)
 {
 	return strcmp(nh_list_char_underlying(a), b);
 }
